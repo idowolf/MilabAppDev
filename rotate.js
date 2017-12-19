dec3c3a1ecd14e2d06359388213c0982d38943a7
Ido Wolf <idowolf@gmail.com>
2017-12-19 22:48:01 +0200

    Empty project in prep for next exercise

--- rotate.js
+++
@@ -1,6 +0,0 @@
-let CONST_STR = "Hello World my name is Ido Wolf";
-for(let i = 0 ; i < CONST_STR.length ; i++) {
-	console.log(CONST_STR);
-	CONST_STR = CONST_STR.slice(-1) +
-		CONST_STR.slice(0, CONST_STR.length - 1);
-}
